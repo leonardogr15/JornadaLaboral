5486a624595c431f6895049b09c90d83b9e4f65f
Your Name <you@example.com>
2025-06-14 18:54:44 -0500

    Feat: adicion boton configuracion

--- frontend/src/pages/PanelPage.js
+++ frontend/src/pages/PanelPage.js
@@ -1,9 +1,22 @@
-import ControlPanel from '../components/ControlPanel'
+import ControlPanel from '../components/ControlPanel';
+import { useNavigate } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi'; 
 
 
 export default function PanelPage() {
+  const navigate = useNavigate();
+
+  const irAConfiguracion = () => {
+    navigate('/configuracion');
+  };
   return (
-    <div style={{ padding: '2rem' }}>
+    <div style={{ padding: '2rem'}}>
+      <div className="panel-header">
+        <button className="config-btn" onClick={irAConfiguracion}>
+          Configuración
+          <FiSettings className="icon" />
+        </button>
+      </div>
       <ControlPanel />
     </div>
   );
